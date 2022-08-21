360093e3c8e184cf3c3cf63a271a5103287c8fc8
wahid022 <rahmanwahid022@gmail.com>
2022-08-21 18:12:16 +0530

    created the schema for register user and implmented their functionality and also created the login controller with proper implementation

--- index.js
+++ index.js
@@ -1,34 +1,84 @@
 const express=require('express');
-const app=express();
-const port=7000;
-const db=require('./config/mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const mongoose=require('mongoose');
+const cors=require('cors');
 
 
-app.use(session(
-    {
-        name:'delivery_system',
-        // TODO change the secret before deployment in production code
-        secret:'blahsomething',
-        saveUninitialized:false,   //todo later
-        resave:false,
-        cookie:
+const app = express()
+const port=8000;
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+
+mongoose.connect("mongodb://localhost:27017/myLoginRegisterDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("DB connected successfully....")
+})
+
+// const userSchema=new mongoose.Schema({
+//     email:String,
+//     password:String
+// })
+
+ 
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+
+const User = new mongoose.model("User", userSchema)
+
+
+app.post("/login",(req,res)=>{
+    console.log(req.body);
+    const {email,password}=req.body
+    
+    User.findOne({email:email},(err,user)=>{
+        if(user)
         {
-            maxAge:(1000*60*100)
-        },
-        store: new MongoStore(
+            if(password===user.password)
             {
-                mongooseConnection: db,
-                autoRemove: 'disabled'
-            
-            },
-            function(err){
-                console.log(err ||  'connect-mongodb setup ok');
+                res.send({message:"Login Successful",user:user})
             }
-        )
+            else{
+                res.send({message:"Password didn't match"})
+            }
+        }
+        else
+        {
+            res.send({message:"User Not Registered"})
+        }
     })
-);
+})
+
+
+app.post("/register", (req, res)=> {
+    console.log("********",req.body);
+    const { name, email, password} = req.body
+    User.findOne({email: email}, (err, user) => {
+        if(user){
+            res.send({message: "User already registerd"})
+        } else {
+            const user = new User({
+                name,
+                email,
+                password
+            })
+            user.save(err => {
+                if(err) {
+                    res.send(err)
+                } else {
+                    res.send( { message: "Successfully Registered, Please login now." })
+                }
+            })
+        }
+    })
+    
+}) 
+
 
 app.get("/",(req,res)=>{
     res.send("app is working")

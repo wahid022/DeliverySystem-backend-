5a8a9274f777b77f1d00faf3635c1f5096bb55e6
wahid022 <rahmanwahid022@gmail.com>
2022-10-09 19:29:53 +0530

    added a new schema for customer

--- index.js
+++ index.js
@@ -26,22 +26,52 @@ mongoose.connect("mongodb://localhost:27017/myLoginRegisterDB", {
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
-    password: String
+    password: String,
+    designation: String
 })
 
 const User = new mongoose.model("User", userSchema)
+
+
+
+
+
+const customerSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    state: String,
+    products:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Products'
+    }
+})
+
+const Customer = new mongoose.model("Customer", customerSchema)
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    brand: String,
+    ccategory: String,
+    description: String
+})
+
+const Product = new mongoose.model("Product", productSchema)
+
 
 
 app.post("/login",(req,res)=>{
     console.log(req.body);
     const {email,password}=req.body
     
-    User.findOne({email:email},(err,user)=>{
+    User.findOne({email:email}, (err,user)=>{
         if(user)
         {
+            
+            console.log("here is the user *********",user);
             if(password===user.password)
             {
-                res.send({message:"Login Successful",user:user})
+                res.send({message: "Login Successfull",user:user})
             }
             else{
                 res.send({message:"Password didn't match"})
@@ -57,27 +87,67 @@ app.post("/login",(req,res)=>{
 
 app.post("/register", (req, res)=> {
     console.log("********",req.body);
-    const { name, email, password} = req.body
+    const { name, email, password,designation} = req.body
     User.findOne({email: email}, (err, user) => {
         if(user){
             res.send({message: "User already registerd"})
         } else {
+            // creating the scema in mongodb for the user ....
             const user = new User({
                 name,
                 email,
-                password
+                password,
+                designation
             })
             user.save(err => {
                 if(err) {
                     res.send(err)
                 } else {
-                    res.send( { message: "Successfully Registered, Please login now." })
+                    res.send( { message: "Successfully Registered, Please login now."})
                 }
             })
         }
     })
     
 }) 
+
+
+app.post("/customer", (req, res)=> {
+    console.log("********",req.body);
+    const { name, email, password,state} = req.body
+    Customer.findOne({email: email}, (err, user) => {
+        if(user){
+            res.send({message: "Customer already registerd with this email"})
+        } else {
+            // creating the scema in mongodb for the user ....
+            const customer = new Customer({
+                name,
+                email,
+                password,
+                state
+            })
+            customer.save(err => {
+                if(err) {
+                    res.send(err)
+                } else {
+                    res.send( { message: "Customer Successfully Registered, Please login now."})
+                }
+            })
+        }
+    })
+    
+}) 
+
+
+
+
+app.post("/products", (req, res)=> {
+
+    console.log("********",req.body);
+    
+}) 
+
+
 
 
 app.get("/",(req,res)=>{
